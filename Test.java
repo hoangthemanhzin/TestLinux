0717e084501d85d1eca58f12518e7e62be2b0acc
hoangthemanhzin <toan6111975@gmail.com>
2022-11-20 11:51:20 +0700

    test

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args){
+        /*
+        * @Author ManhHT
+        */
+        System.out.print("Hello World");
+        System.out.println("xin chao");
+    }
+}
